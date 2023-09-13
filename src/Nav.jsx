184fc71a87863aab1f2c50870c0b3220d3e01997
Napatsawan <napatsawan.az@gmail.com>
2023-09-13 13:14:08 +0700

    edit nav link

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,8 +2,8 @@
 const Nav = () => {
   return (
     <div className="nav">
-      <a href="./">Home</a>
-      <a href="./owner">Owner</a>
+      <a href={"/"}>Home</a>
+      <a href={"/owner"}>Owner</a>
     </div>
   )
 }
